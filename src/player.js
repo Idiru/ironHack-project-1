fa032c7450b99a5606d60758775458217fc31d43
Idir FOUQUET <fouquetidir@gmail.com>
2024-02-17 16:44:57 +0100

    HTML & CSS in progress

--- src/player.js
+++ src/player.js
@@ -1,22 +1,17 @@
-const religionDisplay = document.getElementsByClassName("religion")
-const moneyDisplay = document.getElementsByClassName("money")
+
 
 class Player {
 
     constructor(){
         this.religion = 50
         this.money = 50
-        religionDisplay[0].innerHTML = this.religion
-        moneyDisplay[0].innerHTML = this.money
     }
 
 
     impactCharacteristics(choice, card) { //Method to impact the player's characteristics based on the choice and the current card's characteristics
         console.log(choice)
         this.religion += card.stats[choice].religion //We impact all characteristics based on the choice
-        this.money += card.stats[choice].money
-        religionDisplay[0].innerHTML = this.religion
-        moneyDisplay[0].innerHTML = this.money
+        this.money += card.stats[choice].money¡
         return this // Maybe not needed 
       }
 
